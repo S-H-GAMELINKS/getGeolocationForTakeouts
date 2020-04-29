0b60e4a03d7efaf3ce6adcdad2df32bb5d86983f
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-04-29 22:40:32 +0900

    add main.rb

---
+++ main.rb
@@ -0,0 +1,34 @@
+require "google_maps_service"
+require "dotenv"
+require "json"
+
+Dotenv.load
+
+gmaps = GoogleMapsService::Client.new(key: ENV['API_KEY'])
+
+json = File.read("data.json")
+
+data = JSON.parse(json)
+
+result = File.open("places.json", "a+")
+
+result.write('[')
+
+data.map! {|d|
+    location =  gmaps.geocode(d["address"]).first[:geometry][:bounds][:northeast]
+
+    result.write('
+        {
+            "name": "' + d["name"] + '",
+            "info": "' + d["info"] + '",
+            "address": "' + d["address"] + '",
+            "location": {
+                "latitude": ' + location[:lat].to_s + ',
+                "longitude": ' + location[:lng].to_s + '
+            },
+            "url": "' + d["url"] + '"
+        },
+    ')
+}
+
+result.write(']')
