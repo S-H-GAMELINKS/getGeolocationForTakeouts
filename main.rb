63d3bca9ac9bd345b198ccaeca7fd0a712af59cc
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-04-30 19:04:32 +0900

    add exception handling

--- main.rb
+++ main.rb
@@ -15,20 +15,24 @@ result = File.open("places.json", "a+")
 result.write('[')
 
 data.map! {|d|
-    location =  gmaps.geocode(d["address"]).first[:geometry][:bounds][:northeast]
+    begin 
+        location = gmaps.geocode(d["address"]).first[:geometry][:bounds][:northeast]
 
-    result.write('
-        {
-            "name": "' + d["name"] + '",
-            "info": "' + d["info"] + '",
-            "address": "' + d["address"] + '",
-            "location": {
-                "latitude": ' + location[:lat].to_s + ',
-                "longitude": ' + location[:lng].to_s + '
+        result.write('
+            {
+                "name": "' + d["name"] + '",
+                "info": "' + d["info"] + '",
+                "address": "' + d["address"] + '",
+                "location": {
+                    "latitude": ' + location[:lat].to_s + ',
+                    "longitude": ' + location[:lng].to_s + '
+                },
+                "url": "' + d["url"] + '"
             },
-            "url": "' + d["url"] + '"
-        },
-    ')
+        ')
+    rescue => e
+        puts e.message
+    end
 }
 
 result.write(']')
